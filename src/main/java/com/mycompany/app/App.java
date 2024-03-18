2a40513bba5cb83333a00b8031e3480d2d8b6957
brackenbit <110532594+brackenbit@users.noreply.github.com>
2024-03-18 14:32:12 +1100

    Update App.java
    Report apparent number of cores.

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -11,6 +11,9 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(MESSAGE);
+        // Jenkins is sporadically failing, potentially due to issue with incorrectly
+        // reported number of cores (reporting 16 cores on 12 core system).
+        System.out.println("Available processors: " + Runtime.getRuntime().availableProcessors());
     }
 
     public String getMessage() {
